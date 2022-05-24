4d14b71b2965213f99320aeccc5ad486f3bdd885
raeperd <raeperd117@gmail.com>
2022-05-25 08:53:09 +0900

    Fix datasource configuration error

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("com.h2database:h2")
+    runtimeOnly("com.h2database:h2")
 }
 
 allOpen {
