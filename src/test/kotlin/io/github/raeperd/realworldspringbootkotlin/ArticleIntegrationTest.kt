2c73c16b7b941cb066ecbfb48d79a9d6ff0d5626
raeperd <raeperd117@gmail.com>
2022-06-16 07:03:04 +0900

    #40 Refactor ArticleIntegrationTest

--- src/test/kotlin/io/github/raeperd/realworldspringbootkotlin/ArticleIntegrationTest.kt
+++ src/test/kotlin/io/github/raeperd/realworldspringbootkotlin/ArticleIntegrationTest.kt
@@ -1,14 +1,10 @@
 package io.github.raeperd.realworldspringbootkotlin
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import io.github.raeperd.realworldspringbootkotlin.domain.ArticleDTO
+import io.github.raeperd.realworldspringbootkotlin.domain.ProfileDTO
 import io.github.raeperd.realworldspringbootkotlin.domain.UserDTO
 import io.github.raeperd.realworldspringbootkotlin.domain.slugify
-import io.github.raeperd.realworldspringbootkotlin.util.JpaDatabaseCleanerExtension
-import io.github.raeperd.realworldspringbootkotlin.util.andReturnResponseBody
-import io.github.raeperd.realworldspringbootkotlin.util.notEmptyErrorResponse
-import io.github.raeperd.realworldspringbootkotlin.util.postMockUser
-import io.github.raeperd.realworldspringbootkotlin.util.withAuthToken
+import io.github.raeperd.realworldspringbootkotlin.util.*
 import io.github.raeperd.realworldspringbootkotlin.web.ArticleModel
 import io.github.raeperd.realworldspringbootkotlin.web.ArticlePostDTO
 import io.github.raeperd.realworldspringbootkotlin.web.ArticlePostDTO.ArticlePostDTONested
@@ -21,148 +17,111 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType.APPLICATION_JSON
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.MockMvcResultMatchersDsl
-import org.springframework.test.web.servlet.ResultActionsDsl
-import org.springframework.test.web.servlet.delete
-import org.springframework.test.web.servlet.get
-import org.springframework.test.web.servlet.post
+import org.springframework.test.web.servlet.*
+import java.time.Instant
 
 @ExtendWith(JpaDatabaseCleanerExtension::class)
 @AutoConfigureMockMvc
 @SpringBootTest
 class ArticleIntegrationTest(
-    @Autowired private val mockMvc: MockMvc,
-    @Autowired private val mapper: ObjectMapper
+    @Autowired private val mockMvc: MockMvc
 ) {
-    private val datePattern =
-        Regex("^([+-]?\\d{4}(?!\\d{2}\\b))((-?)((0[1-9]|1[0-2])(\\3([12]\\d|0[1-9]|3[01]))?|W([0-4]\\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\\d|[12]\\d{2}|3([0-5]\\d|6[1-6])))([T\\s]((([01]\\d|2[0-3])((:?)[0-5]\\d)?|24:?00)([.,]\\d+(?!:))?)?(\\17[0-5]\\d([.,]\\d+)?)?([zZ]|([+-])([01]\\d|2[0-3]):?([0-5]\\d)?)?)?)?\$")
-            .toPattern()
-
     @Test
-    fun `when post articles expect return valid json`() {
-        val userDto = mockMvc.postMockUser().andReturnResponseBody<UserModel>().user
-        val dto = articlePostDTOFrom("Some title", "Some description", "Some body", listOf("tag1", "tag2"))
+    fun `when post get delete articles expect return valid response`() {
+        val postDto = createArticlePostDto("Article Title")
 
-        mockMvc.post("/articles") {
-            withAuthToken(userDto.token)
-            contentType = APPLICATION_JSON
-            content = mapper.writeValueAsString(dto)
-        }.andExpect {
-            status { isCreated() }
-            content { validArticleDTO(dto, userDto) }
-        }
-    }
+        mockMvc.getArticlesBySlug(postDto.slug)
+            .andExpect { status { isNotFound() } }
 
-    @Test
-    fun `when get articles by slug expect return valid json`() {
-        val userDto = mockMvc.postMockUser().andReturnResponseBody<UserModel>().user
-        val articleDto = mockMvc.postMockArticle(userDto.token)
-            .andReturnResponseBody<ArticleModel>().article
-
-        mockMvc.get("/articles/not-exists-slug")
+        val author = mockMvc.postMockUser("author")
+        val articleDto = mockMvc.postArticles(author, postDto)
             .andExpect {
-                status { isNotFound() }
-                content { notEmptyErrorResponse() }
-            }
+                status { isCreated() }
+                content { validArticleDTO(postDto, author) }
+            }.andReturnResponseBody<ArticleModel>().article
 
-        mockMvc.get("/articles/${articleDto.slug}")
+        mockMvc.getArticlesBySlug(postDto.slug)
             .andExpect {
                 status { isOk() }
                 content { validArticleDTO(articleDto) }
             }
 
-        mockMvc.delete("/articles/${articleDto.slug}") {
-            withAuthToken(userDto.token)
-        }.andExpect {
-            status { isNoContent() }
-        }
+        mockMvc.deleteArticleBySlug(postDto.slug, author)
+            .andExpect { status { isNoContent() } }
 
-        mockMvc.get("/articles/${articleDto.slug}")
-            .andExpect {
-                status { isNotFound() }
-                content { notEmptyErrorResponse() }
-            }
-    }
-
-    @Test
-    fun `when post favorites article expect return valid json`() {
-        val author = mockMvc.postMockUser().andReturnResponseBody<UserModel>()
-        val dtoBeforeFavorite = mockMvc.postMockArticle(author.user.token)
-            .andReturnResponseBody<ArticleModel>().article
-        val dtoAfterFavorite = dtoBeforeFavorite.copy(favorited = true, favoritesCount = 1)
-
-        mockMvc.post("/articles/${dtoBeforeFavorite.slug}/favorite") {
-            withAuthToken(author.user.token)
-        }.andExpect {
-            status { isOk() }
-            content { validArticleDTO(dtoAfterFavorite) }
-        }
-
-        mockMvc.get("/articles/${dtoBeforeFavorite.slug}") {
-            withAuthToken(author.user.token)
-        }.andExpect {
-            status { isOk() }
-            content { validArticleDTO(dtoAfterFavorite) }
-        }
-
-        mockMvc.delete("/articles/${dtoBeforeFavorite.slug}/favorite") {
-            withAuthToken(author.user.token)
-        }.andExpect {
-            status { isOk() }
-            content { validArticleDTO(dtoBeforeFavorite) }
-        }
-
-        mockMvc.get("/articles/${dtoBeforeFavorite.slug}") {
-            withAuthToken(author.user.token)
-        }.andExpect {
-            status { isOk() }
-            content { validArticleDTO(dtoBeforeFavorite) }
-        }
-    }
-
-    private fun MockMvc.postMockArticle(token: String): ResultActionsDsl {
-        val dto =
-            articlePostDTOFrom("Mocked Title", "mock article description", "mock article body", listOf("mocked-tag"))
-        return post("/articles") {
-            withAuthToken(token)
-            contentType = APPLICATION_JSON
-            content = mapper.writeValueAsString(dto)
-        }
-    }
-
-    private fun articlePostDTOFrom(title: String, description: String, body: String, tags: List<String>) =
-        ArticlePostDTO(ArticlePostDTONested(title, description, body, tags))
-
-    private fun MockMvcResultMatchersDsl.validArticleDTO(dto: ArticlePostDTO, author: UserDTO) {
-        jsonPath("article.slug", equalTo(dto.article.title.slugify()))
-        jsonPath("article.title", equalTo(dto.article.title))
-        jsonPath("article.description", equalTo(dto.article.description))
-        jsonPath("article.body", equalTo(dto.article.body))
-        jsonPath("article.tagList", equalTo(dto.article.tagList))
-        jsonPath("article.createdAt", matchesPattern(datePattern))
-        jsonPath("article.updatedAt", matchesPattern(datePattern))
-        jsonPath("article.favorited", equalTo(false))
-        jsonPath("article.favoritesCount", equalTo(0))
-        jsonPath("article.author.username", equalTo(author.username))
-        jsonPath("article.author.bio", equalTo(author.bio))
-        jsonPath("article.author.image", equalTo(author.image))
-        jsonPath("article.author.following", equalTo(false))
-    }
-
-    private fun MockMvcResultMatchersDsl.validArticleDTO(dto: ArticleDTO) {
-        jsonPath("article.slug", equalTo(dto.slug))
-        jsonPath("article.title", equalTo(dto.title))
-        jsonPath("article.description", equalTo(dto.description))
-        jsonPath("article.body", equalTo(dto.body))
-        jsonPath("article.tagList", equalTo(dto.tagList))
-        jsonPath("article.createdAt", matchesPattern(datePattern))
-        jsonPath("article.updatedAt", matchesPattern(datePattern))
-        jsonPath("article.favorited", equalTo(dto.favorited))
-        jsonPath("article.favoritesCount", equalTo(dto.favoritesCount))
-        jsonPath("article.author.username", equalTo(dto.author.username))
-        jsonPath("article.author.bio", equalTo(dto.author.bio))
-        jsonPath("article.author.image", equalTo(dto.author.image))
-        jsonPath("article.author.following", equalTo(dto.author.following))
+        mockMvc.getArticlesBySlug(postDto.slug)
+            .andExpect { status { isNotFound() } }
     }
 }
+
+fun createArticlePostDto(title: String): ArticlePostDTO {
+    return ArticlePostDTO(
+        ArticlePostDTONested(
+            title = title, description = "Description", body = "Body",
+            tagList = listOf("tag1, tag2")
+        )
+    )
+}
+
+fun MockMvc.postMockUser(username: String): UserDTO =
+    postUsers("${username}@email.com", "password", username)
+        .andReturnResponseBody<UserModel>().user
+
+fun MockMvc.postArticles(author: UserDTO, dto: ArticlePostDTO): ResultActionsDsl =
+    post("/articles") {
+        withAuthToken(author.token)
+        contentType = APPLICATION_JSON
+        accept = APPLICATION_JSON
+        content = SingletonObjectMapper.writeValueAsString(dto)
+    }
+
+fun MockMvc.getArticlesBySlug(slug: String, user: UserDTO? = null): ResultActionsDsl =
+    get("/articles/${slug}") {
+        accept = APPLICATION_JSON
+        user?.let { withAuthToken(user.token) }
+    }
+
+fun MockMvcResultMatchersDsl.validArticleDTO(dto: ArticleDTO) {
+    jsonPath("article.slug", equalTo(dto.slug))
+    jsonPath("article.title", equalTo(dto.title))
+    jsonPath("article.description", equalTo(dto.description))
+    jsonPath("article.body", equalTo(dto.body))
+    jsonPath("article.tagList", equalTo(dto.tagList))
+    jsonPath("article.createdAt", matchesPattern(datePattern))
+    jsonPath("article.updatedAt", matchesPattern(datePattern))
+    jsonPath("article.favorited", equalTo(dto.favorited))
+    jsonPath("article.favoritesCount", equalTo(dto.favoritesCount))
+    jsonPath("article.author.username", equalTo(dto.author.username))
+    jsonPath("article.author.bio", equalTo(dto.author.bio))
+    jsonPath("article.author.image", equalTo(dto.author.image))
+    jsonPath("article.author.following", equalTo(dto.author.following))
+}
+
+private val datePattern =
+    Regex("^([+-]?\\d{4}(?!\\d{2}\\b))((-?)((0[1-9]|1[0-2])(\\3([12]\\d|0[1-9]|3[01]))?|W([0-4]\\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\\d|[12]\\d{2}|3([0-5]\\d|6[1-6])))([T\\s]((([01]\\d|2[0-3])((:?)[0-5]\\d)?|24:?00)([.,]\\d+(?!:))?)?(\\17[0-5]\\d([.,]\\d+)?)?([zZ]|([+-])([01]\\d|2[0-3]):?([0-5]\\d)?)?)?)?\$")
+        .toPattern()
+
+
+private fun MockMvc.deleteArticleBySlug(slug: String, author: UserDTO): ResultActionsDsl =
+    delete("/articles/$slug") {
+        withAuthToken(author.token)
+    }
+
+private val ArticlePostDTO.slug get() = article.title.slugify()
+
+private fun MockMvcResultMatchersDsl.validArticleDTO(dto: ArticlePostDTO, author: UserDTO) {
+    ArticleDTO(
+        slug = dto.slug, title = dto.article.title,
+        description = dto.article.description, body = dto.article.body,
+        tagList = dto.article.tagList,
+        favorited = false, favoritesCount = 0,
+        createdAt = Instant.now(), updatedAt = Instant.now(),
+        author = ProfileDTO(
+            username = author.username,
+            bio = author.bio,
+            image = author.image,
+            following = false
+        )
+    ).let { validArticleDTO(it) }
+}
+
