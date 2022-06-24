362f0c5b8a19bfa4f130388b528e51ddcd92144d
raeperd <raeperd117@gmail.com>
2022-06-25 08:56:27 +0900

    Refactor UserEntity relations

--- src/main/kotlin/io/github/raeperd/realworldspringbootkotlin/infrastructure/jpa/UserEntity.kt
+++ src/main/kotlin/io/github/raeperd/realworldspringbootkotlin/infrastructure/jpa/UserEntity.kt
@@ -4,12 +4,7 @@ import io.github.raeperd.realworldspringbootkotlin.domain.Article
 import io.github.raeperd.realworldspringbootkotlin.domain.Password
 import io.github.raeperd.realworldspringbootkotlin.domain.Profile
 import io.github.raeperd.realworldspringbootkotlin.domain.User
-import javax.persistence.CascadeType
-import javax.persistence.Embedded
-import javax.persistence.Entity
-import javax.persistence.JoinColumn
-import javax.persistence.JoinTable
-import javax.persistence.ManyToMany
+import javax.persistence.*
 
 @Entity
 class UserEntity(
@@ -23,31 +18,29 @@ class UserEntity(
     @Embedded
     override var password: Password,
 
-    @JoinTable(
-        name = "user_followings",
-        joinColumns = [JoinColumn(name = "follower_id", referencedColumnName = "id")],
-        inverseJoinColumns = [JoinColumn(name = "followee_id", referencedColumnName = "id")]
-    )
-    @ManyToMany(cascade = [CascadeType.REMOVE])
-    private var followingUsers: MutableSet<UserEntity> = HashSet()
-) : User, ProfileEntity(id, username, image, bio) {
+    ) : User, ProfileEntity(id, username, image, bio) {
+
+    @ElementCollection
+    @CollectionTable(name = "user_followings", joinColumns = [JoinColumn(name = "follower_id")])
+    @Column(name = "followee_id")
+    private val followingUsers: MutableSet<Long> = HashSet()
 
     override fun viewUserProfile(user: User): Profile {
-        return ProfileEntity(user.id, user.username, user.image, user.bio, followingUsers.contains(user))
+        return ProfileEntity(user.id, user.username, user.image, user.bio, followingUsers.contains(user.id))
     }
 
     override fun followUser(userToFollow: User) {
         if (userToFollow !is UserEntity) {
             throw IllegalArgumentException("Expected UserEntity but given ${userToFollow.javaClass}")
         }
-        followingUsers.add(userToFollow)
+        userToFollow.id?.let { followingUsers.add(it) }
     }
 
     override fun unfollowUser(userToUnFollow: User) {
         if (userToUnFollow !is UserEntity) {
             throw IllegalArgumentException("Expected UserEntity but given ${userToUnFollow.javaClass}")
         }
-        followingUsers.remove(userToUnFollow)
+        userToUnFollow.id?.let { followingUsers.remove(userToUnFollow.id) }
     }
 
     override fun favoriteArticle(article: Article) {
