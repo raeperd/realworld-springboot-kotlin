71439f3a5b490d5f6419460f953c0d5972f6094d
raeperd <raeperd117@gmail.com>
2022-06-30 08:40:35 +0900

    Refactor ProfileIntegrationTest

--- src/test/kotlin/io/github/raeperd/realworldspringbootkotlin/util/spring/ResultMatcherDslExtensions.kt
+++ src/test/kotlin/io/github/raeperd/realworldspringbootkotlin/util/spring/ResultMatcherDslExtensions.kt
@@ -2,13 +2,7 @@ package io.github.raeperd.realworldspringbootkotlin.util.spring
 
 import com.fasterxml.jackson.module.kotlin.readValue
 import io.github.raeperd.realworldspringbootkotlin.util.jackson.SingletonObjectMapper
-import io.github.raeperd.realworldspringbootkotlin.util.jackson.toJson
 import org.springframework.test.web.servlet.ResultActionsDsl
-import org.springframework.test.web.servlet.result.ContentResultMatchersDsl
-
-fun <T> ContentResultMatchersDsl.responseJson(dto: T) {
-    json(dto.toJson())
-}
 
 inline fun <reified T> ResultActionsDsl.andReturnResponseBody(): T {
     return andReturn().response.contentAsString
